8ef73e57ea7bdebcf40e63be77c6fdb2030a7db5
ryansutc <ryansutc@gmail.com>
2016-11-23 09:18:01 -0400

    Update MyListFragment.java
    new Package

--- MyListFragment.java
+++ MyListFragment.java
@@ -1,42 +1,30 @@
-package com.example.w0143446.anotherfragmenttest;
+package com.example.w0143446.pictureviewer;
 
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.Intent;
 import android.content.res.Resources;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class myListFragment extends ListFragment implements OnItemClickListener {
+public class MyListFragment extends ListFragment implements OnItemClickListener {
 
-    //list of images to display
-    //List<String> imagesList = new ArrayList<String>();
-    ListView list;
-    //list adapter for list
-    //ArrayAdapter<String> imagesListAdapter;
-
-    //shared preferences stuff [todo fix up]
+    //shared preferences stuff
     public static final String IMAGE_VIEWED_PREFS = "ImagesViewed";
-    //public Set<String> viewedImgSet; //a set to hold the viewed images for preferences
     public  SharedPreferences settings;
     private Set<String> viewedImgSet;
 
@@ -52,7 +40,7 @@ public class myListFragment extends ListFragment implements OnItemClickListener 
         public void onImageSelected(int index);
     }
 
-    public myListFragment() {
+    public MyListFragment() {
         super();
         // Required empty public constructor
     }
@@ -85,21 +73,10 @@ public class myListFragment extends ListFragment implements OnItemClickListener 
         imageId[4] = R.drawable.pic5;
         imageId[5] = R.drawable.pic6;
         imageId[6] = R.drawable.pic7;
-        //imageId[7] = R.drawable.pic7;
 
         Resources res = getResources();
         web = res.getStringArray(R.array.image_name_arrays);
-        /*
-        web[0] = "Image 1";
-        web[1] = "Image 2";
-        web[2] = "Image 3";
-        web[3] = "Image 4";
-        web[4] = "Image 5";
-        web[5] = "Image 6";
-        web[6] = "Image 7";
-        //web[7] = "Image 8";
-        */
-       // imagesListAdapter = new ArrayAdapter<String>(getActivity(), R.layout.fragment_image, imagesList);
+
         // Restore preferences
         settings = this.getActivity().getPreferences(Context.MODE_PRIVATE); //0 = private
         //populate viewedImgSet with existing values from shared prefs
@@ -164,45 +141,9 @@ public class myListFragment extends ListFragment implements OnItemClickListener 
      */
     public void onListItemClick(ListView l, View v, int position, long id) {
         imgSelectedListener.onImageSelected(position);
-        //showDetails(position);
     }
 
-    /*
-    public void showDetails(int index) {
-        if (mDualPane) {
-            // We can display everything in-place with fragments, so update
-            // the list to highlight the selected item and show the data.
-            getListView().setItemChecked(index, true);
 
-            // Check what fragment is currently shown, replace if needed.
-            ImageFragment details = (ImageFragment)
-                    getFragmentManager().findFragmentById(R.id.tvNote);
-            if (details == null || details.getShownIndex() != index) {
-                // Make new fragment to show this selection.
-                details = ImageFragment.newInstance(index);
-
-                // Execute a transaction, replacing any existing fragment
-                // with this one inside the frame.
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                if (index == 0) {
-                    ft.replace(R.id.tvNote, details);
-                } else {
-                    ft.replace(R.id.tvNote, details);
-                }
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                ft.commit();
-            }
-
-        } else {
-            // Otherwise we need to launch a new activity to display
-            // the dialog fragment with selected text.
-            Intent intent = new Intent();
-            intent.setClass(getActivity(), ImageFragment.class);
-            intent.putExtra("index", index);
-            startActivity(intent);
-        }
-    }
-    */
     @Override
     public void onStop(){
         super.onStop();
